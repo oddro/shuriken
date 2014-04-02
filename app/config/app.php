621f27b74bf76748fb28e2058d0ab2726238849e
Agung Hari Wijaya <a9un9hari@gmail.com>
2014-04-03 06:38:58 +0700

    add cart config

--- app/config/app.php
+++ app/config/app.php
@@ -107,6 +107,7 @@ return array(
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
 		'Cartalyst\Sentry\SentryServiceProvider',
+		'Gloudemans\Shoppingcart\ShoppingcartServiceProvider'
 
 	),
 
@@ -173,7 +174,8 @@ return array(
 		'URL'             => 'Illuminate\Support\Facades\URL',
 		'Validator'       => 'Illuminate\Support\Facades\Validator',
 		'View'            => 'Illuminate\Support\Facades\View',
-		'Sentry' => 'Cartalyst\Sentry\Facades\Laravel\Sentry',
+		'Sentry'		  => 'Cartalyst\Sentry\Facades\Laravel\Sentry',
+		'Cart'            => 'Gloudemans\Shoppingcart\Facades\Cart',
 
 	),
 
